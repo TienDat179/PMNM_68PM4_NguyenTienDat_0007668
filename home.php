4823850547ee72dd5a443762bb50a68d8053e7c1
TienDat179 <raphael2k5@gmail.com>
2026-05-15 11:27:14 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+class home
+{
+    public function index()
+    {
+        echo "day la Trang chủ";
+    }
+
+    public function about()
+    {
+        echo "day la trang ve about";
+
+    }
+
+}
